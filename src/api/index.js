7d08b763b2138301a7c522fefbffcb8875f23cf4
mezzocarattere <annierrosen@gmail.com>
2023-05-03 11:35:22 -0400

    changes server.js to index.js in  package json

--- src/api/index.js
+++ src/api/index.js
@@ -33,13 +33,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../index.html')));
 
-// what is this lol
-app.get('/api', (req, res) => {
-  const path = `/api/item/`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
 
 //GET METRICS ROUTE
 //TO DO: actually build this
